15be48d0da8860236687924b891c1eaffecb4414
2192138785@qq.com <2192138785@qq.com>
2020-02-15 03:24:32 +0800

    rrr

---
+++ src/helpers/AdminLteHelper.php
@@ -0,0 +1,22 @@
+<?php
+namespace diandi\adminlte;
+
+use Yii;
+
+class AdminLteHelper
+{
+    /**
+     * It allows you to get the name of the css class.
+     * You can add the appropriate class to the body tag for dynamic change the template's appearance.
+     * Note: Use this fucntion only if you override the skin through configuration. 
+     * Otherwise you will not get the correct css class of body.
+     * 
+     * @return string
+     */
+    public static function skinClass()
+    {
+        /** @var \dmstr\web\AdminLteAsset $bundle */
+        $bundle = Yii::$app->assetManager->getBundle('diandi\web\AdminLteAsset');
+        return $bundle->skin;
+    }
+}
